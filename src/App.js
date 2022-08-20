aff6ac8eed266a08bc800c05a034182602e99f91
aiyma-kuseinova <aiymaksnva@gmail.com>
2022-08-20 17:59:19 +0600

    hw1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+function AboutUs(){
+    return(
+        <div>
+            <h1>hello world</h1>
+            <p>hello world</p>
+        </div>
+    )
+}
+function Portfolio (){
+    return(
+        <div>
+            <ul>
+                <li>kuseinova</li>
+                <li>aima</li>
+                <li>taalaibekovna</li>
+            </ul>
+        </div>
+    )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <AboutUs/>
+          <Portfolio/>
+      </div>
+  )
 }
 
 export default App;
